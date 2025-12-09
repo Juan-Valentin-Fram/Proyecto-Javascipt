833ca820b95db91e9677468bd5a8695d13675d2a
Juan Fram <juanvalentinfram@gmail.com>
2025-12-09 13:48:09 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// VARIABLE: ESPACIO RESERVADO PARA GUARDAR INFORMACION 
+
+// DECLARACION:
+let cliente;
+
+// ASIGNACION DE VARIABLES:
+cliente = "Carlos";
+console.log(cliente);
+
+// INICIALIZACION DE VARIABLES:
+let edad = 30;
+console.log(edad);
+
+//CONSTANTES: (const) es una variable cuyo valor no puede cambiar
+const nacimiento = 1995;
+console.log(nacimiento);
