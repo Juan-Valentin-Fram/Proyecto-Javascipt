e6fa114e7f738ef5f489713c44cbf8ca91649050
Juan Fram <juanvalentinfram@gmail.com>
2025-12-16 16:48:16 -0300

    Agrege archivos sass y css

--- js/main.js
+++ js/main.js
@@ -24,6 +24,32 @@
 // const nacimiento = 1995;
 // console.log(nacimiento);
 
+// Validacion de Usuarios
+const usuarios = [
+    { nombre: "Ana", edad: 20, aceptoTerminos: false },
+    { nombre: "Luis", edad: 15, aceptoTerminos: true },
+    { nombre: "Carlos", edad: 30, aceptoTerminos: true },
+    { nombre: "Maria", edad: 17, aceptoTerminos: true }
+]
+
+for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].edad >= 18 && usuarios[i].aceptoTerminos) {
+        console.log("Usuario válido: " + usuarios[i].nombre);
+    }
+}
+
+// Ciclos por repeticion
+for (let i = 0; i < 5; i++) {
+    console.log("El valor de i es: " + i);
+}
+
+// Ciclos condicionales
+const password = "1234";
+let passwordIngresada = prompt("Ingrese su contraseña:");
+
+while (passwordIngresada != password) {
+    passwordIngresada = prompt("Contraseña incorrecta. Ingrese su contraseña nuevamente:");
+}
 
 // proyecto final
 
@@ -32,10 +58,10 @@ let objetivo = prompt ("Describa el objetivo de su uso");
 let tecnologias = ["HTML", "CSS", "JavaScript"];
 let escompleto = true;
 
-let numberA = parseInt (prompt("Ingrese un numero para sumar:"));
-let numberB = parseInt (prompt("Ingrese un numero para sumar:"));
-let resultado = numberA + numberB;
-console.log("El resultado de la suma es: " + resultado);
+// let numberA = parseInt (prompt("Ingrese un numero para sumar:"));
+// let numberB = parseInt (prompt("Ingrese un numero para sumar:"));
+// let resultado = numberA + numberB;
+// console.log("El resultado de la suma es: " + resultado);
 
 console.log("Proyecto: " + nombreproyecto + "\nObjetivo: " + objetivo + "\nTecnologias: " + tecnologias + "\nCompletado: " + escompleto);
 
